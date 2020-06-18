dfeb17248fb0aedca69eee5af9bb16930e7a1e86
Antonio Vitor <nP_vitor@hotmail.com>
2020-06-18 13:02:49 -0300

    atualizacao

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,30 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::namespace('Auth')->group(function(){
+    //Login routes
+    Route::get('/login','LoginController@showLoginForm')->name('login');
+    Route::post('/login','LoginController@login');
+    Route::post('/logout','LoginController@logout')->name('logout');
+
+    //Forgot Password Routes
+    Route::get('/password/reset','ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('/password/email','ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+
+    //Reset Password Routes
+    Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'ResetPasswordController@reset')->name('password.update');
+
 });
+
+Route::prefix('/admin')->name('admin.')->namespace('Admin')->group(function(){
+    Route::get('/dashboar','HomeController@index')->name('home');
+});
+
+
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
